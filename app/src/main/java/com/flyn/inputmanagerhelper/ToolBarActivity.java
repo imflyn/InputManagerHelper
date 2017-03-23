afcf914bde111afc6405d21dfdaf5e17a3c8b6e3
imflyn <flyn.yu@fandine.com>
2017-03-23 09:01:59 +0800

    Draw status bar in ToolBarActivity

--- app/src/main/java/com/flyn/inputmanagerhelper/ToolBarActivity.java
+++ app/src/main/java/com/flyn/inputmanagerhelper/ToolBarActivity.java
@@ -1,10 +1,14 @@
 package com.flyn.inputmanagerhelper;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import com.flyn.inputmanagerhelper.helper.InputManagerHelper;
@@ -17,10 +21,15 @@ public class ToolBarActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toolbar);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(ContextCompat.getColor(this, android.R.color.white));
         toolbar.setTitle("THis is toolbar");
         setSupportActionBar(toolbar);
         Button tv_login = (Button) findViewById(R.id.tv_login);
-
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
+        }
         InputManagerHelper.attachToActivity(this).bind((ViewGroup) findViewById(R.id.layout_keyboard), tv_login).offset(16);
     }
 
